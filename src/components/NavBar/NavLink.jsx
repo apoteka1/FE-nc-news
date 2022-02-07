9be00548aa7ae6f9da14ba29b5c8179bd8c5057b
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-02-07 01:47:44 +0000

    filesystem laid out. nav bar wireframed

--- src/components/NavBar/NavLink.jsx
+++ src/components/NavBar/NavLink.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 export const NavLink = ({ path, text }) => {
-    return (
-        <div className="nav__link">
-            <Link to={path}>{text}</Link>
-        </div>
-    );
+	return (
+		<div className="nav__link">
+			<Link to={path}>{text}</Link>
+		</div>
+	);
 };
